0528bb9f0282f5a48cdd8a33f175a3cf095066c4
3hird <dime.neil03@gmail.com>
2025-07-08 00:39:24 +0800

    React Hook From & zod

--- App.tsx
+++ App.tsx
@@ -1,26 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import CustomInput from './src/components/CustomInput';
 import CustomBtn from './src/components/CustomBtn';
+import { useForm } from 'react-hook-form';
+import {z} from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+
+
+const signInSchema = z.object({
+  email: z.string({message:"Email is required"}).email("Invalid email"),
+  password: z.string({message: "Password is required"}).min(8, "Password must be at least 8 characters long"),
+});
+
+type signInFields = z.infer<typeof signInSchema>;
+
+
+
 
 export default function App() {
+
+  const {control, handleSubmit, formState:{errors}} = useForm({
+    resolver: zodResolver(signInSchema),
+  })
+
+  console.log(errors)
+
+  const onSignIn = (data: signInFields) => {
+    console.log("Sign In:", data.email, data.password);
+  }
+
+
+
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}
+     style={styles.container}>
 
       <Text style={styles.title}>Sign In</Text>
 
-      <CustomInput
-        placeholder='Email'
-        autoFocus
-        keyboardType='email-address'
-        autoComplete='email'
-      />
-      <CustomInput placeholder='Password' secureTextEntry/>
-      <CustomBtn onPress={() => console.log("Signing In...")} text='Sign in' />
+      <View style={styles.form}>
+        <CustomInput
+          control={control}
+          name='email'
+          placeholder='Email'
+          autoFocus
+          keyboardType='email-address'
+          autoComplete='email'
+        />
+        <CustomInput control={control} name='password' placeholder='Password' secureTextEntry/>
+      </View>
+      
+      <CustomBtn onPress={handleSubmit(onSignIn)} text='Sign in' />
 
 
       <StatusBar style="auto" /> 
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -36,18 +70,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  // button:{
-  //   backgroundColor: '#0a1282',
-  //   padding: 10,
-  //   borderRadius: 5,
-  //   alignItems: 'center',
-  //   cursor: 'pointer',
-  //   paddingBlock: 12,
-  // }
-  // ,
-  // buttonText: {
-  //   color: '#fff',
-  //   fontSize: 16,
-  // }
-
+  form:{
+    gap: 10
+  }
 });
--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -1,25 +1,43 @@
-import { TextInput, TextInputProps, StyleSheet } from "react-native";
+import { Controller } from "react-hook-form";
+import { TextInput, TextInputProps, StyleSheet, Text, View } from "react-native";
 
 type CustomInputProps = {
-    // custom fields
+    control: any;
+    name: string;
 } & TextInputProps;
 
-export default function CustomInput(props: CustomInputProps){
+export default function CustomInput({control, name, ...props}: CustomInputProps){
     return (
-        <TextInput
-            {...props}
-            style={[styles.input, props.style]}
-            placeholderTextColor={'#a1a1a1'}
-        />
+        <Controller rules={{required: "This field is required"}} control={control} name={name} render={
+        ({field: {value, onChange, onBlur}, fieldState: {error}}) => (
+          <View style={styles.container}>
+            <TextInput
+                value={value}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                {...props}
+                style={[styles.input, props.style]}
+                placeholderTextColor={'#a1a1a1'}
+            />
+            <Text style={styles.error}>{error?.message} </Text>
+          </View>
+        )
+      }/>
     );
 }
 
 const styles = StyleSheet.create({
+    container:{
+        gap: 2,
+    },
     input: {
         borderWidth: 1,
         borderColor: '#a1a1a1',
         padding: 10,
         borderRadius: 5,
     },
+    error: {
+        color: 'crimson',
+    }
 });
 
